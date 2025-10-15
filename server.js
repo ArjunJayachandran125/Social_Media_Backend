7bf031d82472b6443bb435df4bb8fbb89a580061
Arjun Jayachandran <arjunjayan1999@gmail.com>
2025-10-15 12:33:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
